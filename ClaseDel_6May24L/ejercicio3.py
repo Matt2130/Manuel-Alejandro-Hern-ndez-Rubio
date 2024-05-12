ae9d14067a5bb87140a28fed5df3fbdbecd9aff7
Matt2130 <manuel.hdz.rubio07@gmail.com>
2024-05-11 23:07:36 -0600

    primertrabajo

---
+++ ClaseDel_6May24L/ejercicio3.py
@@ -0,0 +1,5 @@
+print("ÁREA DE UN CUADRADO")
+x = float(input("INGRESE EL TAMAÑO DE LA BASE DEL CUADRADO: "))
+y = float(input("INGRESE EL TAMAÑO DE LA ALTURA DEL CUADRADO: "))
+area = x * y 
+print(f"EL ÁREA DEL CUADRADO ES {area}")
