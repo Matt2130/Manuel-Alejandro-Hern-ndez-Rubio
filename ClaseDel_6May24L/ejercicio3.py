c8fdef0909d841b6034ca2f41c63c13a996c1db7
Matt2130 <169078016+Matt2130@users.noreply.github.com>
2024-05-13 08:53:31 -0600

    Delete ClaseDel_6May24L directory

--- ClaseDel_6May24L/ejercicio3.py
+++
@@ -1,5 +0,0 @@
-print("ÁREA DE UN CUADRADO")
-x = float(input("INGRESE EL TAMAÑO DE LA BASE DEL CUADRADO: "))
-y = float(input("INGRESE EL TAMAÑO DE LA ALTURA DEL CUADRADO: "))
-area = x * y 
-print(f"EL ÁREA DEL CUADRADO ES {area}")
